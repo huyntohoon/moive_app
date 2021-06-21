3801fdf25f526ad22dc886f8a6d7241a82908255
huyntohoon <81351526+huyntohoon@users.noreply.github.com>
2021-06-21 17:28:21 +0900

    work_6/21

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React from  'react';
-import PropTypes from "prop-types"
-function Food(){
-  return <h1>HIIIII</h1>
-}
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-      <Food></Food>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="comtainer">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(
+              (
+                movie // movies의 벨류에게 map으로 인자 전체에게 함수 실행(괄호) => 매개변수의 이름
+              ) => (
+                <Movie // JS파일의 movie function 실행
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              )
+            )}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
